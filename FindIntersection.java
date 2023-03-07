063249868d92efd6ae8f8ae67f2300fb6b8e9a81
mkp23 <126979465+mkp23@users.noreply.github.com>
2023-03-08 00:42:59 +0530

    Add files via upload

---
+++ FindIntersection.java
@@ -0,0 +1,37 @@
+package week1.day2;
+
+public class FindIntersection {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int A [] = {3,2,11,4,6,7};
+		
+		int B [] = {1,2,8,4,9,7};
+		
+	for(int i=0; i < A.length; i++) {
+		
+		for(int j=0; j < B.length; j++) {
+			
+			
+			if(A [i] == B [j]) {
+				
+				System.out.println(j);
+			}
+		}
+		
+			
+		
+		
+	}
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
