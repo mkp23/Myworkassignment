fc5698845a35be42273b7aca2f47abca62a3621a
MANISH KUMAR PANDEY <126979465+mkp23@users.noreply.github.com>
2025-01-14 19:34:16 +0530

    Update HelloSelenium.java
    just added some lines

--- HelloSelenium.java
+++ HelloSelenium.java
@@ -4,7 +4,7 @@ public class HelloSelenium {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to TestLeaf to learn Selenium WebDrive");
+		System.out.println("Welcome to TestLeaf to learn Selenium WebDrive and welcome in 2025");
 
 
 	}
